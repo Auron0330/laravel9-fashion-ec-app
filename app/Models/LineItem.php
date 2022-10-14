340c3c0679ab17d6f332d360f7f9f978a9f0bcdd
Hikaru <auron0330@gmail.com>
2022-10-14 20:48:02 +0900

    add chapter-4

---
+++ app/Models/LineItem.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LineItem extends Model
+{
+    use HasFactory;
+    protected $fillable = ['cart_id', 'product_id', 'quantity'];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,25 @@
 <?php
 
 use App\Http\Controllers\ProductController;
- 
+use App\Http\Controllers\LineItemController;
+use App\Http\Controllers\CartController;
+
 Route::controller(ProductController::class)->group(function() {
     Route::name('product.')->group(function () {
         Route::get('/', 'index')->name('index');
         Route::get('/product/{id}', 'show')->name('show');
+    });
+});
+
+Route::controller(LineItemController::class)->group(function() { 
+    Route::name('line_item.')->group(function () {
+        Route::post('/line_item/create', 'create')->name('create');
+        Route::post('/line_item/delete', 'delete')->name('delete');
+    });
+});
+
+Route::controller(CartController::class)->group(function() { 
+    Route::name('cart.')->group(function () {
+        Route::get('/cart', 'index')->name('index');
     });
 });
