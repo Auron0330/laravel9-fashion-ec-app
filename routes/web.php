2a5a376a50403c8eb80eaca0c38ce2a30c22bb31
Hikaru <auron0330@gmail.com>
2022-10-14 20:10:53 +0900

    add chapter-2

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,9 @@
 
 use App\Http\Controllers\ProductController;
  
-Route::get('/', [ProductController::class, 'index'])->name('product.index');
+Route::controller(ProductController::class)->group(function() {
+    Route::name('product.')->group(function () {
+        Route::get('/', 'index')->name('index');
+        Route::get('/product/{id}', 'show')->name('show');
+    });
+});
